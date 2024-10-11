ebae4da85a2fc5ac70de8e3ca5d117a997374d37
sheikh Jawd <136932922+SheikhJawad@users.noreply.github.com>
2024-10-11 18:02:23 +0500

    Update settings.py

--- env/user_management/user_management/settings.py
+++ env/user_management/user_management/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 import os
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 print("ALLOWED_HOSTS:", ALLOWED_HOSTS)  # Add this line to confirm it's being loaded
 
