ca5a100aa39fda02e0c0579d438bf3c19a0166fb
sheikh Jawd <136932922+SheikhJawad@users.noreply.github.com>
2024-10-14 11:34:35 +0500

    Update urls.py

--- env/user_management/user_management/urls.py
+++ env/user_management/user_management/urls.py
@@ -37,7 +37,7 @@ urlpatterns =[
 
         path('admin/', admin.site.urls),
         path('api/',include('user_app.urls')),
-        path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        path('/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
         path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
         path('health/', include('health_check.urls')),
    
