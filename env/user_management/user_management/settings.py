ed1f17d5b4f1a508d7de0fe798ca203e78f7628e
sheikh Jawd <136932922+SheikhJawad@users.noreply.github.com>
2024-10-14 09:27:06 +0500

    Update settings.py

--- env/user_management/user_management/settings.py
+++ env/user_management/user_management/settings.py
@@ -18,6 +18,31 @@ from sentry_sdk.integrations.django import DjangoIntegration
 from dotenv import load_dotenv
 
 
+SECRET_KEY=django-insecure-$988ciceg%_pq0by+5zr0jh0yf8_nkdc4-8))9j%9-qz#n3=e8
+
+
+DEBUG=True
+
+ALLOWED_HOSTS=''
+
+
+DB_ENGINE=django.db.backends.sqlite3
+DB_NAME=db.sqlite3
+
+
+JWT_SIGNING_KEY=django-insecure-$988ciceg%_pq0by+5zr0jh0yf8_nkdc4-8))9j%9-qz#n3=e8
+
+
+EMAIL_BACKEND=django.core.mail.backends.smtp.EmailBackend
+EMAIL_HOST=smtp.gmail.com
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER=hafizjawad858@gmail.com
+EMAIL_HOST_PASSWORD=ezbd tjik wcma gbhg
+
+
+SENTRY_DSN=https://fe973d451a26c2c43a48517c7bf113e6@o4507831910203392.ingest.us.sentry.io/4507831915642880
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # AUTH_USER_MODEL = 'User'
