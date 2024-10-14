eb799dec37d7c6f7247c3ff0ded19f9f659b1b03
sheikh Jawd <136932922+SheikhJawad@users.noreply.github.com>
2024-10-14 10:03:24 +0500

    Update settings.py

--- env/user_management/user_management/settings.py
+++ env/user_management/user_management/settings.py
@@ -42,12 +42,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 
 # Email configuration
-EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "hafizjawad858@gmail.com"
+EMAIL_HOST_PASSWORD = "zsaq aqij mzlj pncv"  # Replace with your App Password
+
 
 # Sentry Configuration
 sentry_sdk.init(
