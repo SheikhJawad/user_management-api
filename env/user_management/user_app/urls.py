45a29e23aacfdb2af4ad45146858f8dc80714414
sheikh Jawd <136932922+SheikhJawad@users.noreply.github.com>
2024-10-17 01:13:02 +0500

    Update urls.py

--- env/user_management/user_app/urls.py
+++ env/user_management/user_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     
-     path('register/', views.RegisterView.as_view(), name='register'),
+     path('register/', views.UserRegisterView.as_view(), name='register'),
      path('login/', views.LoginView.as_view(), name='login'),
      path('logout/',views.LogoutView.as_view(),name='logout'),
      path('password-reset/', views.PasswordResetRequestView.as_view(), name='password_reset_request'),
