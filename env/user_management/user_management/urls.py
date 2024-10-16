a2bafc55a24cc483eeaa155a64b35eb0d26ee1a0
sheikh Jawd <136932922+SheikhJawad@users.noreply.github.com>
2024-10-16 10:57:35 +0500

    Update urls.py

--- env/user_management/user_management/urls.py
+++ env/user_management/user_management/urls.py
@@ -21,16 +21,13 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+    ),
+    public=True,  # Set this to True to allow public access
+    permission_classes=(permissions.AllowAny,),  # Allow anyone to access the schema
 )
     
 urlpatterns =[
