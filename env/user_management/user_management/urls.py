a224dfdde16dceac934adf9c2906f5b2c3f9f7a4
sheikh Jawd <136932922+SheikhJawad@users.noreply.github.com>
2024-10-17 10:17:52 +0500

    Update urls.py

--- env/user_management/user_management/urls.py
+++ env/user_management/user_management/urls.py
@@ -33,7 +33,7 @@ schema_view = get_schema_view(
     
 urlpatterns =[
 
-        path('admin/', admin.site.urls),
+        path('admin12/', admin.site.urls),
         path('api/',include('user_app.urls')),
         path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
         path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
