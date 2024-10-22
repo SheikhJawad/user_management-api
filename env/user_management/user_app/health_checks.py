52ddebe5eb8f9bb7493f66ff1dfe041f782dfd53
sheikh Jawd <136932922+SheikhJawad@users.noreply.github.com>
2024-10-22 10:58:28 +0500

    Update health_checks.py

--- env/user_management/user_app/health_checks.py
+++ env/user_management/user_app/health_checks.py
@@ -3,7 +3,7 @@ from health_check.exceptions import HealthCheckException
 
 class CustomServiceHealthCheck(BaseHealthCheckBackend):
     def check_status(self):
-        # Add your custom check logic here
+   
         if some_service_is_down:
             raise HealthCheckException('Custom service is down')
 
